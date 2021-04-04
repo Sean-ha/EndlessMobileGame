01064291db55f5c4bd0919481e10833288fd80af
Sean <seanha2013@gmail.com>
2021-04-03 23:00:46 -0400

    Initial commit

---
+++ Assets/Scripts/TouchShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchShoot : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(pos), Vector2.zero);
+            // RaycastHit2D can be either true or null, but has an implicit conversion to bool, so we can use it like this
+            if (hitInfo)
+            {
+                Debug.Log(hitInfo.transform.gameObject.name);
+                // Here you can check hitInfo to see which collider has been hit, and act appropriately.
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EndlessMobileGame
+ A current work in progress of an Android game with an endless feedback loop.
